7a309744c178aff19879b85c4f12535771e456c0
awsdevopsrealtime1 <awsdevopsrealtime1@gmail.com>
2025-07-14 13:01:49 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask + PostgreSQL!"
+    return "Hello from Python and Docker-compose"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
